d1c812276101db5ce731a3f423c2ecfc91c87641
Anupam Vishwakarma <160518178+Anupam-Vishwakarma@users.noreply.github.com>
2025-07-10 05:40:27 +0000

    vcariables

---
+++ basics/vaiables.js
@@ -0,0 +1,12 @@
+const accountId=145688
+let accountEmail="user@gmail.com"
+var accountPassword="123465678"
+accountCity="Jaipur"
+let accountState;
+
+//accountId=45 //not allowed
+accountEmail="kk@gmail.com"
+accountPassword="53544"
+accountCity="delhi"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
