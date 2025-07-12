736dac0d512bc1dc9951ae5cb3322b8dffc69aeb
Anupam Vishwakarma <160518178+Anupam-Vishwakarma@users.noreply.github.com>
2025-07-12 05:55:26 +0000

    new file: added

---
+++ basics2/arrays.js
@@ -0,0 +1,17 @@
+const arr =[1,2,3,4,5]
+//const arr2 =new Array(9,8,7,5,6)
+// arr.push(1)
+// arr.pop()
+// const arr2 = arr.join()
+// console.log(arr);
+// console.log(arr2);
+console.log("A",arr);
+const n1 = arr.slice(1,4)
+console.log(n1);
+console.log("B",arr);
+const n2 = arr.splice(1,2)
+console.log("C",arr);
+console.log(n2);
+
+
+
---
+++ basics2/objects.js
@@ -0,0 +1,10 @@
+const jsUser ={
+    name:"krishna",
+    age:18,
+    location:"sln",
+    email:"kk@gmail.com",
+    isLoggedIn :false,
+    lastLoginDays :["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"] 
+}
+
+console.log(jsUser.email);
